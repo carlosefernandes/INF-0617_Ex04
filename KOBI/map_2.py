d6206a005dd2bdf0c60bec0f64cbfa6a6f7a91a8
Carlos Fernandes <carlosf@motorola.com>
2018-10-26 22:41:27 -0300

    Latest changes Carlos

--- KOBI/map_2.py
+++ KOBI/map_2.py
@@ -17,28 +17,39 @@ words = set()
 for i in range(5):
     next(reader)
 
-# process in line
+# Build dictionary of authors indexed by book
 for row in reader :
-	book_authors[row[4]] = row[3].lower()
+	# Consider only book/author whose language is english
+	if row[8] == "":
+		# Replace "," by space in author name
+		book_authors[row[4]] = row[3].lower().replace(","," ")
 
-
+# Build set of popular words
 with open(FILE_COM_WORDS, "r") as f:
 	for line in f:
 		line = line.replace("\n","").replace(" ","").replace("\t","")
 		words.add(line)
 
+# Process lines of books
 for line in sys.stdin:
+	# Manage name of file
 	file = os.environ['mapreduce_map_input_file']
 	prefix, file = file.split("u-")
 	line = re.sub('[^a-z ]', ' ', line.lower())
 	if file not in book_authors.keys(): continue;
+
+	# Get name of author
 	author=book_authors[file]
-	if author not in author_words.keys():
-		author_words[author] = []
-	for word in line.split() :
-		if word not in author_words[author]:
-			author_words[author].append(word)
-			if (word in words):
-				print(author + "," + word + ",0")
-			else:
-				print(author + "," + word + ",1")
+	# Do not consider author Various and Anonymous
+	if author != "Various".lower() and author != "Anonymous".lower():
+		if author not in author_words.keys():
+			author_words[author] = set()
+		for word in line.split() :
+			# Count each word once
+			if word not in author_words[author]:
+				author_words[author].add(word)
+				# Check if word is popular or not
+				if (word in words):
+					print(author + "," + word + ",0")
+				else:
+					print(author + "," + word + ",1")
---
+++ KOBI/run_carlos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#CONFIGURATION PATHS
+WORK_FOLDER=/tmp/data/Trabalho4
+INPUT_FOLDER=/tmp/data/books/txt
+INPUT_INDEX=/tmp/data/master_list.csv
+
+#INITIALIZATION
+OUT_FOLDER=/tmp/data/INF-Trabalho4/output
+rm -rf $OUT_FOLDER
+
+#PART-I
+$HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.0.0.jar \
+-input $INPUT_FOLDER/ \
+-output $OUT_FOLDER/out1 \
+-mapper "python $WORK_FOLDER/map.py" \
+-reducer "python $WORK_FOLDER/reducer.py"
+
+#Case there is more than one reducer - 
+# we should treat the 3000 file selection here
+cat $OUT_FOLDER/out1/* >$OUT_FOLDER/index.txt 
+
+#PART-II
+$HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.0.0.jar \
+-input $INPUT_FOLDER/ \
+-output $OUT_FOLDER/out2 \
+-mapper "python $WORK_FOLDER/map_2.py $INPUT_INDEX $OUT_FOLDER/index.txt" \
+-reducer "python $WORK_FOLDER/reducer_2.py"
+
+cat $OUT_FOLDER/out2/*
